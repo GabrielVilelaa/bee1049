d66fa0611b984a0a77c0f53040b5a1a009003637
gabri <gabrielvilela.acer@gmail.com>
2023-08-07 23:20:11 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        string palavra1, palavra2, palavra3;
+
+        string[] vet = Console.ReadLine().Split(' ');
+        palavra1 = (vet[0]);
+        palavra2 = (vet[1]);
+        palavra3 = (vet[2]);
+
+       
+        if (palavra1 == "vertebrado" )
+        {
+            if (palavra2 == "ave")
+            {
+                if (palavra3 == "carnivoro")
+                {
+                    Console.WriteLine("aguia");
+                }
+               else if (palavra3 == "onivoro")
+                {
+                    Console.WriteLine("pomba");
+                }
+            }
+        } 
+        if (palavra2 == "mamifero")
+        {
+            if (palavra3 == "onivoro")
+            {
+                Console.WriteLine("homem");
+            }
+            else if (palavra3 == "herbivoro")
+            {
+                Console.WriteLine("vaca");
+            }
+        }
+       if ((palavra1 == "invertebrado"))
+        {
+            if (palavra2 == "inseto")
+            {
+                if (palavra3 == "hematofago")
+                {
+                    Console.WriteLine("pulga");
+                }
+                else if (palavra3 == "herbivoro")
+                {
+                    Console.WriteLine("lagarta");
+                }
+            }
+        }
+        if (palavra2 == "anelideo")
+        {
+            if (palavra3 == "hematofago")
+            {
+                Console.WriteLine("sanguessuga");
+            }
+            else if (palavra3 == "onivoro")
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
